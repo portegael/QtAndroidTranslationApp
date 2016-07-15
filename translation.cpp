7d6f81ef0846b8d586678acc69d335f1d877e29f
Gaël Porté <porte.gael@hotmail.fr>
2016-07-15 13:48:23 +0200

    Update translation.cpp

--- translation.cpp
+++ translation.cpp
@@ -1,7 +1,9 @@
 #include "translation.h"
 
-Translation::Translation(QObject *parent) : QObject(parent)
+Translation::Translation(QObject *parent) : QObject(parent);
+    m_language("en")
 {
+    m_translator = new QTranslator(this);
     m_application = (QGuiApplication*)parent;
 }
 
@@ -10,18 +12,26 @@ Translation::Translation(QObject *parent) : QObject(parent)
  */
 void Translation::selectLanguage(QString language)
 {
-    QTranslator qtTranslator;
-
-    QString newLanguage = ":/translations/language_" + language.toLower();
-
-    if( qtTranslator.load(newLanguage) && m_application != NULL)
+if(language != m_language)
     {
-        m_application->installTranslator(&qtTranslator);
-        emit languageChanged();
+        QString newLanguage = ":/translations/language_" + language.toLower();
+
+        m_application->removeTranslator(m_translator);
+
+        if( m_translator->load(newLanguage) && m_application != NULL)
+        {
+            m_language = language;
+            m_application->installTranslator(m_translator);
+            qDebug() << "language" << m_language << "has been installed";
+            emit languageChanged();
+        }
+        else
+        {
+            qWarning() << tr("Cannot load the file") << newLanguage;
+        }
     }
     else
     {
-        qWarning() << "Cannot load the file " << newLanguage;
+        qWarning() << tr("This language is already loaded");
     }
-
 }
